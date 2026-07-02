c7be7b6ebcd5cfccc0dd4e4d5cc2f0561d7a9fee
Kumar Vaibhav <kumarvaibhav.2102@gmail.com>
2026-07-02 23:03:30 +0530

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.cpp
@@ -0,0 +1,32 @@
+class TimeMap {
+    unordered_map<string, vector<pair<string,int>>>mp;
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        const auto& arr = mp[key];
+
+        string ans = "";
+        int i = 0, j = arr.size() - 1;
+
+        while(i <= j ){
+            
+            int mid = i + (j - i)/2;
+
+            if(arr[mid].second <= timestamp){
+            ans = arr[mid].first;
+            i = mid + 1;
+            }else{
+                j = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
